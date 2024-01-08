838f3771e2785daaa37494a61640c61ea811f12c
U1itich <151571667+U1itich@users.noreply.github.com>
2024-01-08 19:45:42 +0300

    Update HW2_2.py

--- HW2_2.py
+++ HW2_2.py
@@ -4,4 +4,4 @@ df = pd.DataFrame({'id':range(1,8),'age':range(1,50,7),'name':['John','Smith','C
 print(df.head(3))
 print(df.tail(3))
 
-df.to_csv("C:\py\DataFrame.csv")
+df.to_csv("C:/py/DataFrame.csv")
