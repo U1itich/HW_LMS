81f87f1065df6d81e56cc0cf93f397424c19a362
U1itich <151571667+U1itich@users.noreply.github.com>
2024-01-08 19:15:08 +0300

    Create HW2_2.py

---
+++ HW2_2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.DataFrame({'id':range(1,8),'age':range(1,50,7),'name':['John','Smith','Carl','Alex','Ann','Jane','Kate']})
+
+print(df.head(3))
+print(df.tail(3))
+
+df.to_csv("C:\py\DataFrame.csv")
