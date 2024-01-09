68a38545cf0853479a8c962538e2597b6ecbaf83
U1itich <151571667+U1itich@users.noreply.github.com>
2024-01-09 17:17:12 +0300

    Update HW2_3.py

--- HW2_3.py
+++ HW2_3.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_csv('/py/Customers.csv')
+df = pd.read_csv('/py/Customers.csv',delimiter=';')
 print(df.query('Age > 30 and Income < 30000'))
 print(df[(df["Work Experience"]>5) & (df["Profession"] == "Lawyer")])
